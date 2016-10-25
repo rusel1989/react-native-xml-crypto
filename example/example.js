7e7e555b0f350a1f359bdc34e46241c5910b1ebf
Jakub Martyčák <martycakjakub@gmail.com>
2016-10-25 20:14:41 +0200

    Add async option and let user specify custom async signer

--- example/example.js
+++ example/example.js
@@ -1,21 +1,21 @@
-var select = require('xml-crypto').xpath
+var select = require('../').xpath
   , dom = require('xmldom').DOMParser
-  , SignedXml = require('xml-crypto').SignedXml
-  , FileKeyInfo = require('xml-crypto').FileKeyInfo  
+  , SignedXml = require('../').SignedXml
+  , FileKeyInfo = require('../').FileKeyInfo
   , fs = require('fs')
 
 function signXml(xml, xpath, key, dest)
 {
   var sig = new SignedXml()
   sig.signingKey = fs.readFileSync(key)
-  sig.addReference(xpath)    
+  sig.addReference(xpath)
   sig.computeSignature(xml)
   fs.writeFileSync(dest, sig.getSignedXml())
 }
 
 function validateXml(xml, key)
 {
-  var doc = new dom().parseFromString(xml)    
+  var doc = new dom().parseFromString(xml)
   var signature = select(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0]
   var sig = new SignedXml()
   sig.keyInfoProvider = new FileKeyInfo(key)
@@ -32,9 +32,9 @@ var xml = "<library>" +
           "</library>"
 
 //sign an xml document
-signXml(xml, 
-  "//*[local-name(.)='book']", 
-  "client.pem", 
+signXml(xml,
+  "//*[local-name(.)='book']",
+  "client.pem",
   "result.xml")
 
 console.log("xml signed succesfully")
